2e5abc482444e7cb1c23497a173ceed9112ea609
yokofly <haohang.shi@timeplus.io>
2025-12-17 21:11:21 -0800

    build fail

--- src/Storages/Stream/StreamingBlockReaderNativeLog.h
+++ src/Storages/Stream/StreamingBlockReaderNativeLog.h
@@ -23,7 +23,6 @@ public:
     /// \param sn fetch start sequence number
     /// \param max_wait_ms_ max wait time for new data per fetch call if there is none
     /// \param queued_max_bytes_ queued bytes for prefetch
-    /// \param fetch_threads_ number of threads to fetch file from shared storage
     StreamingBlockReaderNativeLog(
         Int64 sn,
         Int64 max_wait_ms_,
